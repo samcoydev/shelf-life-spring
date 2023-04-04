da4e5f0b1042762f23385fbfa317951c4994fa48
samcoydev <samuelcoydev@gmail.com>
2023-04-04 17:06:33 -0500

    Init project

---
+++ src/main/java/com/samcodesthings/shelfliferestapi/ShelfLifeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.samcodesthings.shelfliferestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelfLifeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelfLifeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samcodesthings/shelfliferestapi/ShelfLifeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samcodesthings.shelfliferestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfLifeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
