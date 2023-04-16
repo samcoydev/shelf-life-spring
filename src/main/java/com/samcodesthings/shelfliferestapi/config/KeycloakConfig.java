bc289556b1c9f0c84a8fc4a4a5a2390ae8935ff3
samcoydev <samuelcoydev@gmail.com>
2023-04-16 16:30:35 -0500

    Before keycloak changes

---
+++ src/main/java/com/samcodesthings/shelfliferestapi/config/KeycloakConfig.java
@@ -0,0 +1,40 @@
+package com.samcodesthings.shelfliferestapi.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static org.keycloak.OAuth2Constants.CLIENT_CREDENTIALS;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+    @Value("${keycloak.credentials.secret}")
+    private String secretKey;
+    @Value("${keycloak.resource}")
+    private String clientId;
+    @Value("${keycloak.auth-server-url}")
+    private String authUrl;
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(authUrl)
+                .grantType(CLIENT_CREDENTIALS)
+                .realm(realm)
+                .clientId(clientId)
+                .clientSecret(secretKey)
+                .build();
+    }
+}
+
